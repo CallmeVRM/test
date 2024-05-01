2726ed817b17bd0b3817b5b9758ac4a29cdde809
Hamadene Lotfi <106031558+CallmeVRM@users.noreply.github.com>
2024-05-01 16:55:24 +0200

    Update sshconnect.sh

--- sshconnect.sh
+++ sshconnect.sh
@@ -3,6 +3,7 @@
 # Replace with your actual remote server details
 REMOTE_USER="username"
 REMOTE_IP="remote_ip"
+PASSWORD="your_password"
 
-# Execute the reboot command with the secure password
-echo "YourSecurePassword" | ssh "${REMOTE_USER}@${REMOTE_IP}" "sudo reboot"
+# Execute the reboot command with the password
+sshpass -p "$PASSWORD" ssh "${REMOTE_USER}@${REMOTE_IP}" "echo $PASSWORD | sudo -S reboot"
